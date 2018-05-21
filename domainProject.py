79505f2335d1b72469190fe2ff29cb6787b34e13
latimercaleb <nohitokaze@gmail.com>
2018-05-21 08:48:45 -0400

    Domain project complete

--- domainProject.py
+++ domainProject.py
@@ -1,3 +1,6 @@
 #Get email of user
+nameAndEmail = input("What is your email? ")
 #Slice Name from the input email
+nameOnly = nameAndEmail.split('@',2)
 # Print the info of the user
+print("Your name is : ", nameOnly[0])
