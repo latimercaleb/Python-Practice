a4d010249e172d6e80678bfec898fa5279d7048b
Exia <nohitokaze@gmail.com>
2026-01-04 08:15:26 -0500

    Stashing progress

--- Flask Practice/sample.py
+++ Flask Practice/sample.py
@@ -4,3 +4,19 @@ print(2+1)
 
 a = 5
 print(a)
+
+# String practice
+b = "i'm a practice string"
+print(b)
+print(len(b))
+print(b[2])
+
+
+c = b.capitalize()
+print(c)
+
+# Start position included, exit position excluded
+d = c[6:14]
+print(d)
+# Can use format or f-string literals
+print("hello {} world".format(d))
