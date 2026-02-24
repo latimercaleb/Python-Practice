9efb8a538b1c3ad6d06bef3ddbd9eb3d8d25c9c2
Exia <nohitokaze@gmail.com>
2026-02-24 15:37:09 -0500

    WTForm practice

--- Flask Practice/Simple WebPages/wtForms.py
+++ Flask Practice/Simple WebPages/wtForms.py
@@ -10,3 +10,19 @@ class RaiderTemplate(FlaskForm):
     job = StringField("What job are you?")
     role = StringField("What role are you?")
     submit = SubmitField("Submit")
+
+# Routing
+@app.route("/", methods=['GET', 'POST'])
+def home(): 
+    raiderJob = False
+    raiderRole = False
+    raiderForm = RaiderTemplate()
+    if raiderForm.validate_on_submit():
+        raiderJob = raiderForm.job.data
+        raiderRole = raiderForm.role.data
+        raiderForm.job.data = ''
+        raiderForm.role.data = ''
+    return render_template('wtForms-index.html', form=raiderForm, job=raiderJob, role=raiderRole)
+
+if __name__ == '__main__':
+    app.run(debug=True)
