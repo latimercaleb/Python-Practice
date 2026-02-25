382198a4ee79676d8ae4b4948c8a5a2b8fe4f449
Exia <nohitokaze@gmail.com>
2026-02-24 20:05:08 -0500

    Working WTForms, validators & types

--- Flask Practice/Simple WebPages/wtForms.py
+++ Flask Practice/Simple WebPages/wtForms.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import ( StringField, 
+                     SubmitField, 
+                     BooleanField,
+                     DateTimeField, 
+                     RadioField, 
+                     SelectField, 
+                     TextAreaField, 
+                     TextField )
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'no_secrets_today'
@@ -10,6 +18,14 @@ class RaiderTemplate(FlaskForm):
     job = StringField("What job are you?")
     role = StringField("What role are you?")
     submit = SubmitField("Submit")
+
+class StatForm(FlaskForm): # A little more complicated, practicing types
+    strength = StringField("How Strong?", validators=[DataRequired()])
+    dex = BooleanField("Dexterity Type?")
+    magic = RadioField("Magician Type: ", choices=[('magic_type_1','Holy'),('magic_type_2','Void')])
+    enchantment_type = SelectField("Pick a blessing: ", choices=[('enchantment_1','Spark'), ('enchantment_2','Poison'), ('enchantment_3','Recovery')])
+    comments = TextAreaField()
+    submit = SubmitField('Enter')
 
 # Routing
 @app.route("/", methods=['GET', 'POST'])
@@ -23,6 +39,22 @@ def home():
         raiderForm.job.data = ''
         raiderForm.role.data = ''
     return render_template('wtForms-index.html', form=raiderForm, job=raiderJob, role=raiderRole)
+
+@app.route("/stats", methods=['GET', 'POST'])
+def statForm():
+    stats = StatForm()
+    if stats.validate_on_submit(): # stats.strength.data?
+        session['strength'] = stats.data.strength
+        session['dex'] = stats.data.dex
+        session['magic'] = stats.data.magic
+        session['enchantment_type'] = stats.data.enchantment_type
+        session['comments'] = stats.data.comments
+        return redirect(url_for('exit'))
+    return render_template('wtForms-index', form=stats)
+
+@app.route("/thankYou")
+def exit():
+   return render_template('wtForms-thankYou.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
