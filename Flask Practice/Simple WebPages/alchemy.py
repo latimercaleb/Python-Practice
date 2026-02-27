02bdb865acb3d67378758395f7fe65d5493d0d45
Exia <nohitokaze@gmail.com>
2026-02-27 16:27:19 -0500

    Starting relationships & keys review, more later

--- Flask Practice/Simple WebPages/alchemy.py
+++ Flask Practice/Simple WebPages/alchemy.py
@@ -2,21 +2,23 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 
 # Setup 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(base_dir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(base_dir, 'raider-data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 
 db = SQLAlchemy(app)
-Migrate(app,db)
+# Migrate(app,db) Fix locally, need path adjustments
+
 # Initial Model
 class Raider(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     job = db.Column(db.Text)
+    name = db.Column(db.Text)
     level = db.Column(db.Integer)
 
     def __init__(self, job, level):
@@ -25,3 +27,5 @@ class Raider(db.Model):
 
     def __repr__(self): 
         return f"Job is {self.job} with level {self.level}"
+    
+
