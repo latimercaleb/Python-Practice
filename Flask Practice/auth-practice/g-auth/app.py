ef33b8e07c9dc46a369ef6e47669f30d06e40a0a
Exia <nohitokaze@gmail.com>
2026-03-22 14:45:47 -0400

    Integrating google console setup, can fetch google auth data

--- Flask Practice/auth-practice/g-auth/app.py
+++ Flask Practice/auth-practice/g-auth/app.py
@@ -1,15 +1,21 @@
-import os 
+import os
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' # Only for testing purposes, allows for http instead of https
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1' # Only for testing purposes, allows for more relaxed scope checking
 from flask import Flask, redirect, url_for, render_template
 from flask_dance.contrib.google import make_google_blueprint, google # This is the library that provides the OAuth client for Flask
+from dotenv import load_dotenv
 
+load_dotenv()
 app=Flask(__name__)
-app.config['SECRET_KEY'] = 'no_secrets_today'
-app.config['GOOGLE_OAUTH_CLIENT_ID'] = 'your-client-id.apps.googleusercontent.com'
-
-# TODO: Need to setup client & secret later
-blueprint = make_google_blueprint(client_id='', client_secret='', offline=True, scope=['profile', 'email']) # This creates a blueprint for Google OAuth, you need to provide your client id and secret here
+print("Hello creds?")
+g_client_id = os.environ.get('Client_id').strip()
+g_client_secret = os.environ.get('Client_secret').strip()
+print(g_client_id)
+print(g_client_secret)
+app.config['SECRET_KEY'] =  'no_secrets_here'
+app.config['GOOGLE_OAUTH_CLIENT_ID'] = g_client_id # This MUST be set to your Google OAuth client ID, which you can obtain from the Google Developer Console, otherwise app won't work
+# TODO: Need to setup client & secret later, via google developer console, set up gitignore and add to env var for safety practices 
+blueprint = make_google_blueprint(client_id=g_client_id, client_secret=g_client_secret, offline=True, scope=['profile', 'email']) # This creates a blueprint for Google OAuth, you need to provide your client id and secret here
 app.register_blueprint(blueprint, url_prefix='/login') # This registers the blueprint with the Flask app, the url_prefix is where the OAuth routes will be available
 
 @app.route('/')
@@ -32,3 +38,6 @@ def welcome():
     assert response.ok, response.text # Check if the response is successful
     email = response.json()['email'] # Get the user's email from the response
     return render_template('welcome.html', email=email) # Render the welcome page with the user's email
+
+if __name__ == '__main__':
+    app.run(debug=True)
